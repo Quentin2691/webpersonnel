9104f57a7a51dca042f50de99cf51b06445c3718
Quentin Vanderlinden <vdlquentin@hotmail.comvdlquentin@hotmail.comgit config --global user.name QuentinQuentingit commit -m Premier>
2018-09-20 17:11:21 +0200

    personnel

---
+++ dist/assets/js/app.js
@@ -0,0 +1,13 @@
+
+// (function ($) {
+//     $('input.round').wrap('<div class="round"/>').each(function () {
+//         var $input = $(this);
+//         var $div = $input.parent();
+//         var $circle = $('canvas width ="200px" height="200px"/>');
+//         $div.append($circle);
+//         ctx.arc(100, 100, 100, 0, 2 * Math.PI)
+//         ctx.stroke();
+
+
+//     })
+// })(jQuery);
